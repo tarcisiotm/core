5a5f22fd0630d56fbaf3f222866494eebe76c2ba
Tarcísio Costa <tarcisiotm@gmail.com>
2020-04-06 17:18:29 -0700

    -Initial Commit -Pooling Systemm

---
+++ Assets/Scripts/Util/Singleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace TG.Core
+{
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        public bool doNotDestroyOnLoad = false;
+
+        protected static T instance = null;
+
+        public static T I
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        protected virtual void OnEnable(){
+            if (instance != null && instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            instance = this as T;
+
+            if (doNotDestroyOnLoad)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+
+        protected virtual void OnDisable()
+        {
+            if (instance == this)
+            {
+                instance = null;
+            }
+        }
+    }
+}
