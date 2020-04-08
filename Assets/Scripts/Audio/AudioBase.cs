7fa56e04ccb595bb9f7f6cf1b9b0ac63e6481f2e
Tarcísio Costa <tarcisiotm@gmail.com>
2020-04-07 19:32:58 -0700

    feat: audio manager, first iteration

---
+++ Assets/Scripts/Audio/AudioBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TG.Core.Audio {
+    public class AudioBase : MonoBehaviour {
+        protected virtual void OnEnable() {
+            AudioManager.I.AddToAudioList(this);
+        }
+        protected virtual void OnDisable() {
+            AudioManager.I.RemoveFromAudioList(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/PlayAudioAndDisable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace TG.Core.Audio {
+    public class PlayAudioAndDisable : AudioBase {
+
+        [SerializeField] AudioSource audioSource;
+        Transform targetTransform;
+
+        float duration;
+
+        protected virtual void Update() {
+            duration -= Time.deltaTime;
+            if (duration < 0) { HandleDisableObject(); }
+
+            if(targetTransform != null) {
+                transform.position = targetTransform.position;
+            }
+        }
+
+        public virtual void PlayAndDisable(AudioClip audioClip, float volume = 1f, Transform transformToFollow = null) {
+            audioSource.clip = audioClip;
+            audioSource.volume = volume;
+            audioSource.Play();
+            duration = audioClip.length + 1f;
+
+            targetTransform = transformToFollow;
+        }
+
+        public virtual void HandleDisableObject() {
+            targetTransform = null;
+            gameObject.SetActive(false);
+        }
+    }
+}
