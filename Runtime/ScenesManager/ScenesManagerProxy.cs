ba95075dbea094a11821861f2c6be71726d991a6
Tarcísio Costa <tarcisiotm@gmail.com>
2020-04-13 11:12:21 -0700

    chore: updated scenes manager proxy

--- Runtime/ScenesManager/ScenesManagerProxy.cs
+++ Runtime/ScenesManager/ScenesManagerProxy.cs
@@ -8,22 +8,17 @@ namespace TG.Core {
     /// Scenes Manager Proxy for operations that require a reference
     /// </summary>
     public class ScenesManagerProxy : MonoBehaviour {
-        ScenesManager scenesManager;
-
-        protected virtual void Start() {
-            scenesManager = FindObjectOfType<ScenesManager>();
-        }
 
         public virtual void LoadMainMenu() {
-            scenesManager.LoadMainMenu();
+            ScenesManager.I.LoadMainMenu();
         }
 
         public virtual void LoadNextLevel() {
-            scenesManager.LoadNextSceneWithFade();
+            ScenesManager.I.LoadNextSceneWithFade();
         }
 
         public virtual void ReloadScene() {
-            scenesManager.ReloadScene();
+            ScenesManager.I.ReloadScene();
         }
 
     }
