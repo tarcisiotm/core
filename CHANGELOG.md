136603c46be9e96837eeb40e0e8ac9e58d026642
Tarcísio Costa <tarcisiotm@gmail.com>
2022-07-16 10:05:56 -0700

    Updated package

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,9 @@ All notable changes to this package will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 
+## [0.6.3] - 2021-05-13
+### Fixed package updater.
+
 ## [0.6.2] - 2021-05-13
 ### Fixed unload issue with ScenesManager.
 
--- Editor/PackageUpdater.cs
+++ Editor/PackageUpdater.cs
@@ -3,43 +3,46 @@ using System.IO;
 using System.Text;
 using UnityEditor;
 
-public class PackageUpdater {
-
-    [MenuItem("Tools/TG Core/Refresh TG Core Package")]
-    static void RefreshPackage() {
-
-        string path = "Assets/../Packages/manifest.json";
-        string lockGitPackageLine = "    \"com.tarcisiogames.core\": {";
-        string hashReplace = "\"hash\" : \"\"";
-
+public class PackageUpdater
+{
+    [MenuItem("Tools/TG Core/Refresh TG Core Package 2")]
+    static void RefreshPackage()
+    {
+        string path = "Assets/../Packages/packages-lock.json";
+        string lockGitPackageLine = "com.tarcisiogames.core";
+        string hashReplace = "      \"hash\" : \"\"";
         bool hasSeenPackage = false;
         bool isDone = false;
 
         StringBuilder sb = new StringBuilder();
 
-        using (StreamReader reader = new StreamReader(path)) {
-            while (true) {
+        using (StreamReader reader = new StreamReader(path))
+        {
+            while (true)
+            {
                 string line = reader.ReadLine();
 
                 if (line == null) { break; }
 
-                if (!isDone && hasSeenPackage && line.Contains("hash")) {
+                if (!isDone && hasSeenPackage && line.Contains("hash"))
+                {
                     line = hashReplace;
                     isDone = true;
                 }
 
-                if (!hasSeenPackage && line.Contains(lockGitPackageLine)) {
+                if (!hasSeenPackage && line.Contains(lockGitPackageLine))
+                {
                     hasSeenPackage = true;
                 }
                 sb.Append(line + Environment.NewLine);
             }
         }
 
-        using (StreamWriter fileWriter = File.CreateText(path)) {
+        using (StreamWriter fileWriter = File.CreateText(path))
+        {
             fileWriter.Write(sb);
         }
 
         AssetDatabase.Refresh();
     }
-
 }
